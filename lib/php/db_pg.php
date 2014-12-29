92acffaf6229695354e823e52e4f6d90d5201956
antho101 <rg512@live.be>
2014-12-29 23:48:47 +0100

    Update db_pg.php

--- lib/php/db_pg.php
+++ lib/php/db_pg.php
@@ -1,5 +1,5 @@
 <?php
-$dsn="pgsql:host=lamp-edu.condorcet.be;dbname=anthony_lattuca_projet;port=5432";
+$dsn="pgsql:host=lamp-edu.ecole.be;dbname=fake;port=5432";
 $user="anthony.lattuca";
-$pass="oracle";
+$pass="fake";
 ?>
